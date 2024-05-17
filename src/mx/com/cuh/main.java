770bec0f23770f06ea60af4bfd7963373ba10671
Carlos Ruhal Sebastian Ruiz Perez <sebastianperezruiz6@gmail.com>
2024-05-17 10:23:49 -0600

    Primer commit

--- src/mx/com/cuh/main.java
+++ src/mx/com/cuh/main.java
@@ -3,9 +3,8 @@ package mx.com.cuh;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
-		
-		
+		System.out.println("Hola"); 
 	}
+     //system.out.println("x");
 
 }
