cccf7daf29a820dd9dae7709e8ba881426258262
Pedrito Sola <sebastianperezruiz6@gmail.com>
2024-05-20 10:20:23 -0600

    merge

--- src/mx/com/cuh/main.java
+++ src/mx/com/cuh/main.java
@@ -3,8 +3,11 @@ package mx.com.cuh;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola"); 
-		System.out.println("Adios");
+		System.out.println("Hola");
+		System.out.println("Adios");System.out.println("Adios");
+
+		String cosa = "hola";
+		System.out.println("como estas");
 		
 	}
 
