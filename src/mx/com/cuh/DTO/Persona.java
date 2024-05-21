c2cba1c7cbb816dee883b199360b3050f0bc2693
Sebastian Perez Ruiz <30965966+mrrobotoo@users.noreply.github.com>
2024-05-21 10:56:44 -0600

    Add files via upload

---
+++ src/mx/com/cuh/DTO/Persona.java
@@ -0,0 +1,52 @@
+package mx.com.cuh.DTO;
+
+public class Persona {
+	private String colorCabello;
+	private String colorOjos;
+	private String colorPiel;
+	private Float altura;
+	private Float peso;
+	private String nacionalidad;
+	
+	
+	public String getColorCabello() {
+		return colorCabello;
+	}
+	public void setColorCabello(String colorCabello) {
+		this.colorCabello = colorCabello;
+	}
+	
+	public String getColorOjos() {
+		return colorOjos;
+	}
+	public void setColorOjos(String colorOjos) {
+		this.colorOjos = colorOjos;
+	}
+	public String getColorPiel() {
+		return colorPiel;
+	}
+	public void setColorPiel(String colorPiel) {
+		this.colorPiel = colorPiel;
+	}
+	public Float getAltura() {
+		return altura;
+	}
+	public void setAltura(Float altura) {
+		this.altura = altura;
+	}
+	public Float getPeso() {
+		return peso;
+	}
+	public void setPeso(Float peso) {
+		this.peso = peso;
+	}
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	
+	
+	
+}
