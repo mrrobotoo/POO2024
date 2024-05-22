2c9a7e66db738b66eb3c5c384067f65f4b7cf33f
OSCAR\lunap <cuh51229508@cuh.mx>
2024-05-21 22:49:40 -0600

    Se cambio a perro

--- src/mx/com/cuh/main.java
+++ src/mx/com/cuh/main.java
@@ -1,4 +1,4 @@
-package mx.com.cuh;
+/*package mx.com.cuh;
 
 import mx.com.cuh.DTO.Persona;
 
@@ -7,16 +7,43 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		
-		Persona persona1 = new Persona();
-		persona1.setColorCabello("Castaño");
-		persona1.setAltura(1.70F);
-		persona1.setColorOjos("cafe");
-		persona1.setColorPiel("prieto");
-		persona1.setNacionalidad("peruano");
-		persona1.setPeso(70F);
+		Perro perro1 = new Persona();
+		perro1.setColorCabello("Castaño");
+		perro1.setAltura(1.70F);
+		perro1.setColorOjos("cafe");
+		perro1.setColorPiel("prieto");
+		perro1.setNacionalidad("peruano");
+		perro1.setPeso(70F);
 		
 		
-		System.out.println("El color de piel es " + persona1.getColorPiel());
+		System.out.println("El color de piel es " + perro1.getColorPiel());
+		System.out.println(agregarInformacion("cola"));
+	}
+	
+	private static  String agregarInformacion(String nombre) {
+		return nombre;
+	} 
+*/
+}
+package mx.com.cuh;
+
+import mx.com.cuh.DTO.Perro;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		
+		Perro perro1 = new Perro();
+		perro1.setColorCabello("Castaño");
+		perro1.setAltura(1.70F);
+		perro1.setColorOjos("cafe");
+		perro1.setColorPiel("prieto");
+		perro1.setNacionalidad("peruano");
+		perro1.setPeso(70F);
+		
+		
+		System.out.println("El color de piel es " + perro1.getColorPiel());
 		System.out.println(agregarInformacion("cola"));
 	}
 	
