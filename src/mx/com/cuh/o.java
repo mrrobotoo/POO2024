86373569304c64b307119569c88a60ebba2cda0b
DESKTOP-1CKME85\loco_ <loco_marte@hotmail.com>
2024-05-22 07:30:08 +0200

    listo creo

---
+++ src/mx/com/cuh/o.java
@@ -0,0 +1,35 @@
+package mx.com.cuh;
+
+public class o {
+	private String raza;
+	private String tamaño;
+	private String peso;
+	private String color;
+	
+	public String getraza() {
+		return raza;
+	}
+	public void setraza(String raza) {
+		this.raza = raza;
+	}
+	public String gettamaño() {
+		return tamaño;
+	}
+	public void settamaño(String tamaño) {
+		this.tamaño = tamaño;
+	}
+	public String getpeso() {
+		return peso;
+	}
+	public void setpeso(String peso) {
+		this.peso = peso;
+	}
+	public String getcolor() {
+		return color;
+	}
+	public void setcolor(String color) {
+		this.color = color;
+	}
+
+
+	}
---
+++ src/mx/com/cuh/oo.java
@@ -0,0 +1,15 @@
+package mx.com.cuh;
+
+public class oo {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		
+		o oo = new o ();
+		oo.setcolor("negro");
+		oo.setpeso("2.5f");
+		oo.setraza("pastor");
+		oo.settamaño("pequeño");
+	}
+
+}
