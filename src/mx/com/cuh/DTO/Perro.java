d01c476b0c5e27a5fa2a08d99d8ba954b7e34084
MIRANDA <miranda200341@gmail.com>
2024-05-22 09:59:42 -0600

    correcionPerro

---
+++ src/mx/com/cuh/DTO/Perro.java
@@ -0,0 +1,41 @@
+package mx.com.cuh.DTO;
+
+public class Perro {
+
+	private String razaDelPerro;
+	private String colorPelo;
+	private String tamaño;
+	private float edad;
+	private String sexo;
+	public String getRazaDelPerro() {
+		return razaDelPerro;
+	}
+	public void setRazaDelPerro(String razaDelPerro) {
+		this.razaDelPerro = razaDelPerro;
+	}
+	public String getColorPelo() {
+		return colorPelo;
+	}
+	public void setColorPelo(String colorPelo) {
+		this.colorPelo = colorPelo;
+	}
+	public String getTamaño() {
+		return tamaño;
+	}
+	public void setTamaño(String tamaño) {
+		this.tamaño = tamaño;
+	}
+	public float getEdad() {
+		return edad;
+	}
+	public void setEdad(float edad) {
+		this.edad = edad;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+}
