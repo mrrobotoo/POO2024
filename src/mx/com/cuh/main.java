d21e1908b1fd46dd723da84362738fbc8496bd40
LEduardo25 <97324291+LEduardo25@users.noreply.github.com>
2024-05-27 10:18:20 -0600

    Se actualiza calculadora

--- src/mx/com/cuh/main.java
+++ src/mx/com/cuh/main.java
@@ -9,17 +9,25 @@ public class main {
 		float num2 =6;
 		
 		System.out.println("Calculadora 1.Suma, 2. Resta, 3.Division, 4.multiplicación, 5. Exponente");
-		int opcion =6;
+		int opcion =1;
 		
 		if (opcion ==1) {
 			System.out.println(num1 + num2);
-		}else if (opcion ==2) {
+		}
+		else if (opcion ==2) {
 			System.out.println(num1 - num2); 
 		}
-		 
+		else if (opcion ==3) {
+			System.out.println(num1 / num2); 
+		}
+		else if (opcion ==4) {
+			System.out.println(num1 * num2); 
+		}
+		else if (opcion ==5) {
+			System.out.println(Math.pow(num1,num2));
+		}
 		
-		System.out.println(num1 / num2); 
-		System.out.println(Math.pow(num1,num2)); 
+		
 	}
 	
 	
