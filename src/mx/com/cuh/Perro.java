e1425db544b4b9ef5508e3a456bd48536de86296
LENOVO8\loreh <yespachecohdez04@gmail.com>
2024-05-27 10:06:20 -0600

    .

---
+++ src/mx/com/cuh/Perro.java
@@ -0,0 +1,41 @@
+package mx.com.cuh;
+
+public class Perro {
+	private String Raza;
+	private String Color;
+	private Float Altura;
+	private Float Peso;
+	
+	
+	public String getRaza() {
+		return Raza;
+	}
+	public void setRaza(String raza) {
+		Raza = raza;
+		
+	}
+	public String getColor() {
+		return Color;
+	}
+	public void setColor(String color) {
+		Color = color;
+		
+	}
+	public Float getAltura() {
+		return Altura;
+	}
+	public void setAltura(Float altura) {
+		Altura = altura;
+		
+	}
+	public Float getPeso() {
+		return Peso;
+	}
+	public void setPeso(Float peso) {
+		Peso = peso;
+	}
+	
+	
+	
+
+}
