1ffc8b70496b577d40f7a476235a85ca92dc12d6
DESKTOP-1CKME85\loco_ <loco_marte@hotmail.com>
2024-05-28 09:46:58 +0200

    ya quedo

--- src/mx/com/cuh/main.java
+++ src/mx/com/cuh/main.java
@@ -1,30 +1,41 @@
 package mx.com.cuh;
 
+import java.util.Scanner;
+
 import mx.com.cuh.DTO.Persona;
 
 public class main {
 
+	static Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		float num1 =5;
-		float num2 =6;
+		System.out.println("Ingresa el primer numero pay:");
+		int num1 = scanner.nextInt();
+
+		System.out.println("Ingresa el primer numero pay:");
+		int num2 = scanner.nextInt();
 		
 		System.out.println("Calculadora 1.Suma, 2. Resta, 3.Division, 4.multiplicación, 5. Exponente");
-		int opcion =6;
+		int opcion = scanner.nextInt();
 		
 		if (opcion ==1) {
 			System.out.println(num1 + num2);
+			
 		}else if (opcion ==2) {
 			System.out.println(num1 - num2); 
+			
 		}else if (opcion ==3) {
 			System.out.println(num1 / num2);
+			
 		}else if (opcion ==4) {
-			System.out.println(num1 *= num2);
+			System.out.println(num1 * num2);
+			
 		}else if (opcion ==5) {
 			System.out.println(Math.pow(num1,num2)); 
 			
-		
+			
 		}
-		
+	
 	}
 	
 	
